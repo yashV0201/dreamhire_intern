24afca1db7a639aa3affb0a19c79e8ea24cc49b5
yashV0201 <157841419+yashV0201@users.noreply.github.com>
2024-11-21 19:54:03 +0530

    Add files via upload

---
+++ test-resume/src/main/java/com/dreamhire/test_resume/model/TestResume.java
@@ -0,0 +1,13 @@
+package com.dreamhire.test_resume.model;
+
+public class TestResume {
+
+    private int id;
+
+    private String fileName;
+
+    private String profileName;
+
+    private String data;
+
+}
---
+++ test-resume/src/test/java/com/dreamhire/test_resume/TestResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamhire.test_resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
